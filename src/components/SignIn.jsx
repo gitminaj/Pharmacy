d5c48ad78b5b665082f5d01181d74d4490a9ccbe
Minaj <minajinamdar9@gmail.com>
2024-10-09 20:47:38 +0530

    updated folder structure

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -7,13 +7,15 @@ const SignIn = () => {
 
   <div className="h-[70%] w-[60%] bg-white flex items-center justify-evenly rounded-md shadow-2xl">
 
-            <div className='input-section w-[40%] h-[60%] flex justify-center'>
+            <div className='input-section w-[40%] h-[80%] flex justify-center'>
                 <form onSubmit={(e) => e.preventDefault()} className="p-4 flex flex-col justify-evenly">
-                    <h1 className="font-bold text-2xl"> Sign In </h1>
+                    <h1 className="font-bold text-2xl"> Login </h1>
 
                     <input className="h-[15%] rounded p-2 border-black border-b-4 focus:outline-none" type="text" placeholder="Username" required></input>
                     <input className="h-[15%] rounded p-2 border-black border-b-4 focus:outline-none" type="password" placeholder="password" required></input>
-                    <button className="rounded border-2 border-black p-2 font-bold hover:bg-pink-400">SignIn</button>
+                    <button className="rounded border-2 border-black p-2 font-bold bg-pink-300 hover:bg-pink-400">Login</button>
+                    <h2>Forgot Password?</h2>
+                    <h2>New here?</h2>
 
                 </form>
             
---
+++ src/components/medical_specialist/medicalSpecailistDashboard.jsx
@@ -0,0 +1,9 @@
+
+
+const medical_specailist_Dashboard = () => {
+  return (
+    <div>UserDashboard</div>
+  )
+}
+
+export default medical_specailist_Dashboard;
